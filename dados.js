231bc1e4716e672dd4de6ac0bc457738c110ae8e
Karinekim <77906751+karinekim@users.noreply.github.com>
2024-09-07 23:09:02 -0400

    primeira versão
    primeira versão

---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+    {
+        titulo: "Cattleya labiata",
+        descricao: "A Cattleya labiata é uma das orquídeas mais famosas e apreciadas do Brasil. Suas flores são grandes, perfumadas e possuem uma incrível variedade de cores, sendo muito procurada por colecionadores.",
+        link: "https://pt.wikipedia.org/wiki/Cattleya_labiata",
+        tags: "orquídea cattleya labiata perfumada",
+        imagem: "https://upload.wikimedia.org/wikipedia/commons/a/a1/Cattleya_labiata.jpg"
+    },
+    {
+        titulo: "Phalaenopsis amabilis",
+        descricao: "A Phalaenopsis amabilis, também conhecida como 'orquídea borboleta', é uma orquídea nativa do sudeste asiático. É amplamente cultivada devido às suas flores elegantes e duradouras, que podem durar meses.",
+        link: "https://pt.wikipedia.org/wiki/Phalaenopsis_amabilis",
+        tags: "orquídea phalaenopsis borboleta duradoura",
+        imagem: "https://upload.wikimedia.org/wikipedia/commons/8/8f/Phalaenopsis_amabilis.jpg"
+    },
+    {
+        titulo: "Oncidium",
+        descricao: "Oncidium é um gênero de orquídeas popularmente conhecido como 'chuva de ouro'. Suas pequenas flores amarelas crescem em cachos e são muito apreciadas pela sua beleza e facilidade de cultivo.",
+        link: "https://pt.wikipedia.org/wiki/Oncidium",
+        tags: "orquídea Oncidium chuva de ouro",
+        imagem: "https://upload.wikimedia.org/wikipedia/commons/e/e2/Oncidium_sharry_baby.JPG"
+    }
+];
